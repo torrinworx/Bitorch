bbd9447d2174e169661ab22b297dc999460b58cf
torrin1 <torrin@worx4you.com>
2024-01-12 12:08:10 -0500

    Fixing mongo pex stuff

--- backend/api/pex/pex_mongo.py
+++ backend/api/pex/pex_mongo.py
@@ -1,59 +1,58 @@
 from typing import List, Optional
 from utils.mongo import mongo_manager
 
+mongo_manager = mongo_manager
+collection_name = "peers"
+
 
 class PexMongo:
-    def __init__(self):
-        self.mongo_manager = mongo_manager
-        self.collection_name = "peers"
-
-    async def add_peers(self, peer_list: List[dict]) -> bool:
+    @staticmethod
+    async def add_peers(peer_list: List[dict]) -> bool:
         """
         Add list of peers to the database.
         """
         success = True
         for peer in peer_list:
-            peer_exists = await self.mongo_manager.find_documents(
-                self.collection_name, peer
-            )
+            peer_exists = await mongo_manager.find_documents(collection_name, peer)
             if not peer_exists:
-                await self.mongo_manager.insert_document(self.collection_name, peer)
+                await mongo_manager.insert_document(collection_name, peer)
             else:
                 success = False
         return success
 
-    async def add_peer(self, peer_info: dict) -> bool:
+    @staticmethod
+    async def add_peer(peer_info: dict) -> bool:
         """
         Add a new peer to the database.
         """
-        existing_peer = await self.mongo_manager.find_documents(
-            self.collection_name, peer_info
-        )
+        existing_peer = await mongo_manager.find_documents(collection_name, peer_info)
         if existing_peer:
             return False
-        await self.mongo_manager.insert_document(self.collection_name, peer_info)
+        await mongo_manager.insert_document(collection_name, peer_info)
         return True
 
-    async def get_all_peers(self) -> List[dict]:
+    @staticmethod
+    async def get_all_peers() -> List[dict]:
         """
         Retrieve all peers from the database.
         """
-        return await self.mongo_manager.find_documents(self.collection_name, {})
+        return await mongo_manager.find_documents(collection_name, {})
 
-    async def remove_peer(self, peer_info: dict) -> bool:
+    @staticmethod
+    async def remove_peer(peer_info: dict) -> bool:
         """
         Remove a peer from the database.
         """
-        return await self.mongo_manager.delete_document(self.collection_name, peer_info)
+        return await mongo_manager.delete_document(collection_name, peer_info)
 
+    @staticmethod
     async def update_peer(
         self, old_peer_info: dict, new_peer_info: dict
     ) -> Optional[dict]:
         """
         Update a peer's peer_info.
         """
-        result = await self.mongo_manager.update_document(
-            self.collection_name, old_peer_info, new_peer_info
+        result = await mongo_manager.update_document(
+            collection_name, old_peer_info, new_peer_info
         )
         return new_peer_info if result else None
-
